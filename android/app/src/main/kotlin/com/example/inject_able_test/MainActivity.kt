8eda014074472927ccf447a5f9785aba5fb7c329
yazan99sh <syria.yazan19@gmail.com>
2021-06-04 16:42:17 +0300

    first commit

---
+++ android/app/src/main/kotlin/com/example/inject_able_test/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.inject_able_test
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/di/di_config.config.dart
@@ -0,0 +1,21 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+// **************************************************************************
+// InjectableConfigGenerator
+// **************************************************************************
+
+import 'package:get_it/get_it.dart' as _i1;
+import 'package:injectable/injectable.dart' as _i2;
+
+import '../main.dart' as _i3; // ignore_for_file: unnecessary_lambdas
+
+// ignore_for_file: lines_longer_than_80_chars
+/// initializes the registration of provided dependencies inside of [GetIt]
+_i1.GetIt $initGetIt(_i1.GetIt get,
+    {String? environment, _i2.EnvironmentFilter? environmentFilter}) {
+  final gh = _i2.GetItHelper(get, environment, environmentFilter);
+  gh.factory<_i3.Test2>(() => _i3.Test2());
+  gh.factory<_i3.Test>(() => _i3.Test(get<_i3.Test2>()));
+  gh.factory<_i3.MyApp>(() => _i3.MyApp(get<_i3.Test>()));
+  return get;
+}
